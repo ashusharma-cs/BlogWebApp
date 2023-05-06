251ae235f44842a3200f3b37525334d46fee4111
ashusharma-cs <ashu.lucifer@gmail.com>
2023-05-06 14:55:59 -0700

    Inital Commit

---
+++ app.js
@@ -0,0 +1,105 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://0.0.0.0:27017/blogDB", { useNewUrlParser: true });
+
+
+const aboutContent = "This is a Blog Web Application built to store and compose posts. This application is developed using NodeJS, Express, Template Engine(EJS) and MongoDB. To compose a post, the user has to hit /compose on the url to visit the compose route. Once a user submits the post the data is stored in a mongoDB database. All the posts can be viewed on the home route.";
+const contactContent = "Developed By: Ashu Sharma";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+
+
+  Post.find()
+    .then((post) => {
+
+      res.render("home", {
+        posts: post
+
+      });
+    })
+
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+
+
+  const post = new Post({
+
+    title: req.body.postTitle,
+
+    content: req.body.postBody
+
+  });
+
+
+  post.save()
+
+
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId })
+    .then((post) => {
+      res.render("post", {
+
+        title: post.title,
+
+        content: post.content
+
+      });
+    })
+
+
+
+});
+
+
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
